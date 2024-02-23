f7317676ced6b33425e679b877cfcd1c2edd1360
Waelkp <waelkp187@gmail.com>
2024-02-23 21:12:26 +0300

    Completely Finished1

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ coptButton.onclick = function () {
   navigator.clipboard.writeText("Waelalhouri04@gmail.com");
   coptButton.textContent = "Copied!";
   setTimeout(() => {
-              coptButton.textContent = "Copy";
-              console.log("Copy");
+    cHeoptButton.textContent = "Copy";
+    console.log("Copy");
   }, 5000);
 };
 function copyText() {
   /* Copy text into clipboard */
 }
 
+console.log("Hello I am Wael  19 years old");
